2c842a4a6096223eed637bededa168d835fc7796
Steven Henry <stevennassef97@gmail.com>
2020-02-21 13:46:44 +0200

    1- Created Spells Scriptable Objects

---
+++ Assets/_Scripts/Scriptable Objects/Spell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Spells/New Spell")]
+public class Spell : ScriptableObject
+{
+    [SerializeField] private Spells _spellName;
+    [ColorUsage(true, true)] [SerializeField] private Color _spellColor;
+    [SerializeField] private Sprite _spellSymbol;
+
+    public Spells SpellName() => _spellName;
+    public Color SpellColor() => _spellColor;
+    public Sprite SpellSymbol() => _spellSymbol;
+
+}
+
+public enum Spells
+{
+    Death_Hole,Telekinesis,Creator,Manipulator,Soul_Shifter
+}
