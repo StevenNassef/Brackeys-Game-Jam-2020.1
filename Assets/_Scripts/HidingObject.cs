feae47426d67e12c8a542d8b19e10061c0907ef2
Basem Rizk <basem.rizk@outlook.com>
2020-02-22 21:28:38 +0200

    Add ObjectHider in vcam in Omar

---
+++ Assets/_Scripts/HidingObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player.Control;
+
+public class HidingObject : MonoBehaviour
+{
+    public Material _transparent;
+
+    private Material _original;
+
+    public GameObject player;
+    private PlayerControllerEngine playerControllerEngine;
+    private Renderer rend;
+ 
+    void Start()
+    {
+        // player = GameObject.FindWithTag("Player");
+        playerControllerEngine = player.GetComponent<PlayerControllerEngine>();
+        rend = GetComponentInChildren<Renderer>();
+    }
+
+    void Update()
+    {
+        if ((transform.position - Camera.main.transform.position).sqrMagnitude < playerControllerEngine.GetDistanceFromScreen())
+        {
+            _original = rend.material;
+            rend.material = _transparent;
+        }
+        else
+        {
+            rend.material = _original;
+        }
+    }
+ 
+}
+
