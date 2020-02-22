73a75417fb42da655a14c4d2cfc4a5f607fe9266
unknown <ibram.medhat2@gmail.com>
2020-02-22 16:06:57 +0200

    Controlling walking and idle animations through patrol script

--- Assets/_Scripts/Enemy Controls/Patrol.cs
+++ Assets/_Scripts/Enemy Controls/Patrol.cs
@@ -13,15 +13,15 @@
         public GameObject target;
         private bool targetDetected = false;
         public float speedFactorChange = 2.0f;
-
         public float deltaHeightThereshold = 0.5f;
         void Start () {
             agent = GetComponent<NavMeshAgent>();
             fov = GetComponent<FieldOfView>();
+            GetComponent<Animator>().SetBool("IsIdle", true);
             // Disabling auto-braking allows for continuous movement
             // between points (ie, the agent doesn't slow down as it
             // approaches a destination point).
-            agent.autoBraking = true;
+            agent.autoBraking = false;
 
             GotoNextPoint();
         }
@@ -34,16 +34,15 @@
 
             // Set the agent to go to the currently selected destination.
             agent.destination = points[destPoint].position;
-
+            GetComponent<Animator>().SetBool("IsIdle", false);
             // Choose the next point in the array as the destination,
             // cycling to the start if necessary.
             destPoint = (destPoint + 1) % points.Length;
         }
 
         void OnCollisionEnter(Collision other) {
-            //Debug.Log("Gameover!");
-            // Checking if the collision occurs with layer 11(Player) then gameover
 
+            // Checking if the collision occurs by comparing with the Player tag
             float deltaHeight = Math.Abs(other.transform.position.y - transform.position.y);
             if(other.gameObject.CompareTag("Player") && deltaHeight < deltaHeightThereshold) {
                 // Call Gameover Logic
@@ -59,6 +58,7 @@
                     agent.speed = agent.speed * speedFactorChange;
                     targetDetected = true;
                 }
+                GetComponent<Animator>().SetBool("IsIdle", false);
             }
             else if (!agent.pathPending && agent.remainingDistance < 1f) {
                 if(targetDetected) {
