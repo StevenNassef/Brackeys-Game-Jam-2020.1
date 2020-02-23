bc84c95be442f99986f1b0df21160ab7ddeb27ef
Michael Khalil <mikegeorge2012@gmail.com>
2020-02-23 03:34:52 +0200

    add enemy sound

--- Assets/_Scripts/Audio/GeneralSounds.cs
+++ Assets/_Scripts/Audio/GeneralSounds.cs
@@ -30,70 +30,70 @@ public class GeneralSounds : MonoBehaviour
     }
 
     [ButtonMethod]
-    void playZombieSound() {
+    public void playZombieSound() {
         playBackground(zombieSound, true);
     }
 
     [ButtonMethod]
-    void playDeathSound() {
+    public void playDeathSound() {
         playBackground(deathCharacterSound, false);
     }
 
     [ButtonMethod]
-    void playGruntCharacterSound() {
+    public void playGruntCharacterSound() {
         playEffect(gruntCharacterSound, false);
     }
 
     [ButtonMethod]
-    void playColorSound() {
+    public void playColorSound() {
         playEffect(colorSound, false);
     }
 
     [ButtonMethod]
-    void playCubeSound() {
+    public void playCubeSound() {
         playEffect(cubeSound, false);
     }
 
     [ButtonMethod]
-    void playTileActivateSound() {
+    public void playTileActivateSound() {
         playEffect(tileActivateSound, false);
     }
 
     [ButtonMethod]
-    void playPortalSound() {
+    public void playPortalSound() {
         playEffect(portalSound, false);
     }
 
     [ButtonMethod]
-    void playMusic() {
+    public void playMusic() {
         playMusic(musicTrack, true);
     }
 
-    void playEffect(AudioClip clip, bool loop) {
+    public void playEffect(AudioClip clip, bool loop) {
         effect.GetComponent<AudioSource>().loop = loop;
         effect.GetComponent<AudioSource>().clip = clip;
         effect.GetComponent<AudioSource>().Play();
     }
 
-    void playBackground(AudioClip clip, bool loop) {
+    public void playBackground(AudioClip clip, bool loop) {
         background.GetComponent<AudioSource>().loop = loop;
         background.GetComponent<AudioSource>().clip = clip;
         background.GetComponent<AudioSource>().Play();
     }
 
-    void playSFX(AudioClip clip, bool loop) {
+    public void playSFX(AudioClip clip, bool loop) {
         sfx.GetComponent<AudioSource>().loop = loop;
         sfx.GetComponent<AudioSource>().clip = clip;
         sfx.GetComponent<AudioSource>().Play();
     }
 
-    void playMusic(AudioClip clip, bool loop) {
+    public void playMusic(AudioClip clip, bool loop) {
         music.GetComponent<AudioSource>().loop = loop;
         music.GetComponent<AudioSource>().clip = clip;
         music.GetComponent<AudioSource>().Play();
     }
 
-    void playMaster(AudioClip clip, bool loop) {
+    public void playMaster(AudioClip clip, bool loop) {
         master.GetComponent<AudioSource>().loop = loop;
         master.GetComponent<AudioSource>().clip = clip;
         master.GetComponent<AudioSource>().Play();
@@ -101,31 +101,31 @@ public class GeneralSounds : MonoBehaviour
     
 
     [ButtonMethod]
-    void stopEffect(){
+    public void stopEffect(){
         effect.GetComponent<AudioSource>().loop = false;
         effect.GetComponent<AudioSource>().Stop();
     }
 
     [ButtonMethod]
-    void stopBackground(){
+    public void stopBackground(){
         background.GetComponent<AudioSource>().loop = false;
         background.GetComponent<AudioSource>().Stop();
     }
 
     [ButtonMethod]
-    void stopSFX(){
+    public void stopSFX(){
         sfx.GetComponent<AudioSource>().loop = false;
         sfx.GetComponent<AudioSource>().Stop();
     }
     
     [ButtonMethod]
-    void stopMusic(){
+    public void stopMusic(){
         music.GetComponent<AudioSource>().loop = false;
         music.GetComponent<AudioSource>().Stop();
     }
 
     [ButtonMethod]
-    void stopMaster(){
+    public void stopMaster(){
         master.GetComponent<AudioSource>().loop = false;
         master.GetComponent<AudioSource>().Stop();
     }
